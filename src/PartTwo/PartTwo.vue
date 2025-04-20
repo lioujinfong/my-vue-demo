f59b143da2220d71903af184b0d2ca6f0b64080a
劉謹逢 <47555881+lioujinfong@users.noreply.github.com>
2025-04-20 20:25:59 +0800

    Update PartTwo.vue

--- src/PartTwo/PartTwo.vue
+++ src/PartTwo/PartTwo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="iframe-container">
     <iframe title="demo_2"
-      src="https://app.powerbi.com/view?r=eyJrIjoiOGVlOTcxYjYtZGNhZS00MTI0LWI0N2EtMzhiZWZlNWRlY2M4IiwidCI6IjFlZjg1NDQ4LTIwYzItNDQwYS04NzFkLTRmM2RlZjExMDQyZiIsImMiOjEwfQ%3D%3D"
+      src="https://app.powerbi.com/view?r=eyJrIjoiZjE2OTM1YzUtZWM4ZS00MjYxLWE0ZjMtODhlZWI2NWYxNDVmIiwidCI6IjFlZjg1NDQ4LTIwYzItNDQwYS04NzFkLTRmM2RlZjExMDQyZiIsImMiOjEwfQ%3D%3D"
       frameborder="0" allowFullScreen="true"></iframe>
   </div>
 </template>
